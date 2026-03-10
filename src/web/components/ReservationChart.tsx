d307f6512e175709ec845f316c770588cca5f0a7
robertpezdirc <robertpezdirc@gmail.com>
2026-03-10 21:36:17 +0100

    "perf-add-lazy-loading-for-charts

--- src/web/components/ReservationChart.tsx
+++ src/web/components/ReservationChart.tsx
@@ -1,18 +1,37 @@
 "use client";
 
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-  Cell,
-  PieChart,
-  Pie,
-  Legend,
-} from "recharts";
+import dynamic from 'next/dynamic';
+
+// Lazy load recharts components (saves ~150KB on initial load)
+const ResponsiveContainer = dynamic(
+  () => import('recharts').then(mod => mod.ResponsiveContainer),
+  { ssr: false }
+);
+
+const PieChart = dynamic(
+  () => import('recharts').then(mod => mod.PieChart),
+  { ssr: false }
+);
+
+const Pie = dynamic(
+  () => import('recharts').then(mod => mod.Pie),
+  { ssr: false }
+);
+
+const Cell = dynamic(
+  () => import('recharts').then(mod => mod.Cell),
+  { ssr: false }
+);
+
+const Tooltip = dynamic(
+  () => import('recharts').then(mod => mod.Tooltip),
+  { ssr: false }
+);
+
+const Legend = dynamic(
+  () => import('recharts').then(mod => mod.Legend),
+  { ssr: false }
+);
 
 interface ReservationChartProps {
   data: {
